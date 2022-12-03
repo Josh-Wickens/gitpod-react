a7d91797ebaf2d8eb47b6ba2f9116ffc2a8cc3cc
Josh-Wickens <joshwickens@hotmail.com>
2022-12-03 13:37:29 +0000

    start of conditional Rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsClass from './components/EventsClass';
+import EventsFunctional from './components/EventsFunctional';
+import EventBinding from './components/EventBinding';
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+
 
 function App() {
   return (
@@ -9,6 +15,11 @@ function App() {
       <FunctionalGreeting />
       <FunctionalGreetingWithProps />
       <StatefulGreetingWithPrevState />
+      <EventsFunctional />
+      <EventsClass />
+      <EventBinding />
+      <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional />
     </div>
   );
 }
