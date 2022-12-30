8a48f609d8f45854cf5c0a6545dbc59d04f68baf
Josh-Wickens <joshwickens@hotmail.com>
2022-12-30 16:08:24 +0000

    conditional effects

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+// import UseEffectCounter from './components/UseEffectCounter';
 // import FunctionalGreeting from './components/FunctionalGreeting';
 // import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 // import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
@@ -16,9 +17,11 @@ import './App.css';
 // import ControlledForm from './components/ControlledForm';
 // import UncontrolledForm from './components/UncontrolledForm';
 // import HooksCounter from "./components/HooksCounter";
-// import ClassCounter from "./components/ClassCounter";
+import ClassCounter from "./components/ClassCounter";
 // import ControlledFormHooks from './components/ControlledFormHooks';
-import UseStateWithArrays from './components/UseStateWithArrays';
+// import UseStateWithArrays from './components/UseStateWithArrays';
+// import UseStateWithObjects from './components/UseStateWithObjects';
+import UseEffectCounter from './components/UseEffectCounter';
 
 function App() {
   return (
@@ -40,9 +43,11 @@ function App() {
       <ControlledForm />
       <UncontrolledForm />
       <HooksCounter />
+      <ControlledFormHooks /> 
+      <UseStateWithArrays /> 
+      <UseStateWithObjects /> */}
       <ClassCounter /> 
-      <ControlledFormHooks /> */}
-      <UseStateWithArrays />
+      <UseEffectCounter />
     
 
       
